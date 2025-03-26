da0a2bf097153ff2c91b85e2648c6a63ae29766f
luckyyudha <lucky.yudhadibrata@gmail.com>
2025-03-26 08:50:11 +0700

    update js1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 //ini javascript ekternal
+function hitungLuas() {
+    let sisi = document.getElementById("sisi").value;
+    
+    if (sisi === "" || sisi <= 0) {
+        document.getElementById("hasil").innerHTML = "Masukkan angka yang valid!";
+        return;
+    }
+    
+    let luas = sisi * sisi;
+    document.getElementById("hasil").innerHTML = "Luas persegi: " + luas + " cm²";
+}
+
+function hitungKeliling() {
+    let sisi = document.getElementById("sisi").value;
+    
+    if (sisi === "" || sisi <= 0) {
+        document.getElementById("hasil").innerHTML = "Masukkan angka yang valid!";
+        return;
+    }
+    
+    let keliling = 4 * sisi;
+    document.getElementById("hasil").innerHTML = "Keliling persegi: " + keliling + " cm";
+}
